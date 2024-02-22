daa7c201bfbed48a71d3ff0a064305abc41d63ef
Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2024-02-21 23:41:54 -0800

    Create taskOne.cpp

---
+++ Lecture_02/Task_01/taskOne.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e9+7;
+#define PI acos(-1)
+
+int main(){
+    for(int i=10; i<=90; ++i){
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+    int i=10;
+    while(i<=90){
+        cout<<i<<" ";
+        ++i;
+    }
+    cout<<"\n";
+    int k=10;
+    do{
+        cout<<k<<" ";
+        k++;
+    }while(k<=90);
+}

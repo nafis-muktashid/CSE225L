454e826ff8c42e74e149f2985020987b8599cd92
Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2024-02-21 23:43:46 -0800

    Update taskTwo.cpp

--- Lecture_02/Task_02/taskTwo.cpp
+++ Lecture_02/Task_02/taskTwo.cpp
@@ -3,20 +3,46 @@ using namespace std;
 const int N = 1e9+7;
 #define PI acos(-1)
 
+bool even(int a){
+    if(a%2==0) return true;
+    else return false;
+}
+
 int main(){
-    for(int i=10; i<=90; ++i){
-        cout<<i<<" ";
+    int n; cin>>n;
+
+    if(even(n)) cout<<"Number is Even"<<"\n";
+    else cout<<"Number is odd"<<"\n";
+
+    bool isPrimeFor = true;
+    for(int i=2; i<=n/2; ++i){
+        if(n%i==0){
+            isPrimeFor = false;
+             break;
+        }
     }
-    cout<<"\n";
-    int i=10;
-    while(i<=90){
-        cout<<i<<" ";
+    (isPrimeFor)? cout<<"Prime using for loop."<<"\n" : cout<<"Not Prime using for Loop"<<"\n";
+
+    bool isPrimeWhile = true;
+    int i=2;
+    while(i<=n/2){
+        if(n%i==0){
+            isPrimeWhile = false;
+            break;
+        }
         ++i;
     }
-    cout<<"\n";
-    int k=10;
+    (isPrimeWhile)? cout<<"Prime using while loop."<<"\n" : cout<<"Not Prime using while Loop"<<"\n";
+
+    bool isPrimeDoWhile = true;
+    int k=2;
     do{
-        cout<<k<<" ";
-        k++;
-    }while(k<=90);
+        if(n%i==0){
+            isPrimeDoWhile = false;
+            break;
+        }
+        ++i;
+    }while(i<=n/2);
+    (isPrimeDoWhile)? cout<<"Prime using while loop."<<"\n" : cout<<"Not Prime using while Loop"<<"\n";
+
 }

bbea7ac13459c9b7557e0efcbca0d921af704640
Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2024-03-07 00:49:32 -0800

    Create arr.h

---
+++ Lecture_04/Task_01/arr.h
@@ -0,0 +1,17 @@
+#ifndef ARR_H_INCLUDED
+#define ARR_H_INCLUDED
+
+class arr{
+private:
+    int n;
+    int *vec;
+public:
+    arr();
+    arr(int);
+    void setValues(int, int);
+    int getValues(int);
+
+
+};
+
+#endif // ARR_H_INCLUDED

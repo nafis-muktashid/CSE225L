a5e4a0860dbb5dede531cf54a6d4584c506cd281
Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2024-04-02 18:41:16 +0600

    Create sortedtype.h

---
+++ Graded_Assignment/List/LinkedList_Based/Sorted_List/sortedtype.h
@@ -0,0 +1,29 @@
+#ifndef SORTEDTYPE_H_INCLUDED
+#define SORTEDTYPE_H_INCLUDED
+
+template <class T>
+class SortedType{
+    struct NodeType{
+        T info;
+        NodeType* next;
+    };
+
+public:
+    SortedType();
+    ~SortedType();
+    bool IsFull();
+    int LengthIs();
+    void MakeEmpty();
+    void RetrieveItem(T& , bool&);
+    void InsertItem(T);
+    void DeleteItem(T);
+    void ResetList();
+    void GetNextItem(T&);
+
+private:
+    NodeType* listData;
+    int length;
+    NodeType* currentPos;
+};
+
+#endif // SORTEDTYPE_H_INCLUDED
